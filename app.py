494e09a89355041c8dfe16e30f96e02356174f1a
Anika Zolman-Nacey <49991173+AnikaZN@users.noreply.github.com>
2024-06-06 10:12:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ elif choice == "About":
     st.subheader("Some basic facts about what we do!")
     st.balloons()
 
-elif choice == "Upcoming Events":
+elif choice == "Upcoming Events" and authentication_status == False:
     st.header("When to find us!")
 
     calendar_options = {"selectable": True}
